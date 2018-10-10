523f96ae200b59518b61dbbc87c0452ab290405b
Geoff Lambert <admin@vulpineinteractive.com>
2018-10-09 23:18:15 -0600

    added dependencies

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+$(function () {
+   
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAowIUNPenp2Xyc5LZj860wwzjvPZF6NMU",
+        authDomain: "rps-online-d42fe.firebaseapp.com",
+        databaseURL: "https://rps-online-d42fe.firebaseio.com",
+        projectId: "rps-online-d42fe",
+        storageBucket: "rps-online-d42fe.appspot.com",
+        messagingSenderId: "559255222867"
+    };
+    firebase.initializeApp(config);
+});
